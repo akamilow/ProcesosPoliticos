4239d40e17acb777ae579265527d3566de623695
Camilo <camilocastelar3@gmail.com>
2024-04-28 18:32:09 -0500

    Avance: primera ventana de la interfaz grafica

--- src/co/edu/ude/poo/procesospoliticos/Principal.java
+++ src/co/edu/ude/poo/procesospoliticos/Principal.java
@@ -1,5 +1,6 @@
 package co.edu.ude.poo.procesospoliticos;
 
+import co.edu.ude.poo.procesospoliticos.vistas.gui.VentanaPrincipal;
 
 import co.edu.ude.poo.procesospoliticos.modelo.entidades.Candidato;
 import co.edu.ude.poo.procesospoliticos.modelo.entidades.Ciudadano;
@@ -14,6 +15,12 @@ import co.edu.ude.poo.procesospoliticos.modelo.crud.VocalMesaCrud;
 public class Principal {
     
     public static void main(String[] args) {
+        
+        VentanaPrincipal v1 = new VentanaPrincipal();
+        v1.setLocationRelativeTo(null);
+        //v1.setExtendedState(VentanaPrincipal.MAXIMIZED_BOTH);
+        v1.setVisible(true);
+        
         Partido partido1 = new Partido("Partido liberal");
         Partido partido2 = new Partido("Partido conservador");
         Partido partido3 = new Partido("Partido verde");
